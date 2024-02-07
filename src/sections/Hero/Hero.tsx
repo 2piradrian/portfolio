3451702690804f2f353288c76389ba244d4d10b8
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-07 17:57:07 -0300

    Add Hero component to Home page and update header styles

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import style from "./style.module.css";
+
+export default function Hero() {
+	return (
+		<div className={style.container}>
+			<h1 className={style.name}>Adrián Rodriguez</h1>
+		</div>
+	);
+}
