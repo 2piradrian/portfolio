5190642a48ea485a2b4c10cb004d15f0fc927240
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-07 18:16:27 -0300

    Add contact information to Hero section

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import me from "../../assets/page/me.svg";
 import world from "../../assets/page/world.svg";
 import style from "./style.module.css";
 
 export default function Hero() {
 	return (
 		<div className={style.container}>
+			<Image src={me} alt="Me" />
 			<h1 className={style.name}>Adrián Rodriguez</h1>
 			<p className={style.about}>Desarrollador Full Stack, y estudiante de ingeniería.</p>
 			<div className={style.location}>
 				<Image src={world} alt="World" />
 				<p>Córdoba, Argentina.</p>
+			</div>
+			<div className={style.contact}>
+				<Link href="mailto:rodriguezcadr@gmail.com">Contáctame</Link>
+				<Link href="https://www.linkedin.com/in/rodriguezcadr/">Linkedin</Link>
 			</div>
 		</div>
 	);
