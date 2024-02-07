ac23ac2cbf76d3eec396a404f15b1aee5a2cd25d
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-07 18:04:12 -0300

    Delete SVG files and update styles in header and navbar components

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import world from "../../assets/page/world.svg";
 import style from "./style.module.css";
 
 export default function Hero() {
 	return (
 		<div className={style.container}>
 			<h1 className={style.name}>Adrián Rodriguez</h1>
+			<p className={style.about}>Desarrollador Full Stack, y estudiante de ingeniería.</p>
+			<div className={style.location}>
+				<Image src={world} alt="World" />
+				<p>Córdoba, Argentina.</p>
+			</div>
 		</div>
 	);
 }
