1ea7feb1a5997c9210ec282208e40afc497119e7
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-07 17:40:08 -0300

    project setup

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import NavItem from "./NavItem";
+import style from "./style.module.css";
+
+export default function Navbar() {
+	return (
+		<div>
+			<ul className={style.navList}>
+				<NavItem href="/#projects" arialabel="Proyectos" title="Proyectos" />
+				<NavItem href="/#skills" arialabel="Habilidades" title="Habilidades" />
+				<NavItem href="/#education" arialabel="Educación" title="Educación" />
+				<NavItem href="/#contact" arialabel="Contacto" title="Contacto" />
+			</ul>
+		</div>
+	);
+}
