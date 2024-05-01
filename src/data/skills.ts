3075cd33309c0486d9e2f97740c7ace3f7b8c84e
Adrian Rodriguez <83842205+2piradrian@users.noreply.github.com>
2024-04-30 22:42:31 -0300

    skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,6 +1,6 @@
 export const skills: string[][] = [
 	["HTML", "CSS", "JavaScript", "React.js", "Next.js"],
 	["TypeScript", "Node.js", "Express.js", "MongoDB", "PostgreSQL"],
-	["Kotlin", "Jetpack Compose", "Java", "Spring Boot"],
-	["Git", "GitHub", "Álgebra de Boole"],
+	["Kotlin", "Jetpack Compose"],
+	["Git", "GitHub", "Docker", "Álgebra de Boole"],
 ];
