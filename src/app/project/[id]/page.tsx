7c5a0935a3576100b2d8771bc60f6c88afb85218
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-26 22:51:01 -0300

    Remove unused code and update project details in Project page

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,9 +1,7 @@
-"use client";
 import ProjectDetails from "@/sections/ProjectDetails/ProjectDetails";
 import projects from "@/public/data/projects.json";
 
 export default function Project({ params }: { params: { id: string } }) {
-	// get the url id
 	const projectId = parseInt(params.id as string);
 
 	return (
